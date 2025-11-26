2850103ecfbbb547ab87430d6ed48151ff877919
Ashley S. <foxhound118451@gmail.com>
2025-11-26 01:44:04 -0800

    Begun work on material parsing.

---
+++ src/material.cpp
@@ -0,0 +1,59 @@
+#include <material.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+//Prototypes
+int parse_material(Material* material);
+
+char line[256]; //current line being read
+ifstream file;
+
+vector<Material> parse_mtl(const char* path)
+{
+	vector<Material> materials;
+	file = ifstream(path);
+	if (!file)
+	{
+		cerr << "ERROR: Failed opening material file at path: " << path << endl;
+		return materials;
+	}
+
+	while (!(file.eof() || file.fail())) //parse by line
+	{
+		file.getline(line, 256);
+
+		char* strtok_state{};
+
+		char* line_type = strtok_s(line, " ", &strtok_state);
+		if (strcmp(line_type, "newmtl"))
+		{
+			//parse material
+			Material material{};
+			if(parse_material(&material))
+			{
+				materials.push_back(material);
+			}
+			else
+			{
+				cerr << "ERROR: Failed parsing material with name: \"" << material.name << "\"" << endl;
+				return materials;
+			}
+		}
+		else if (line_type[0] != '#')
+		{
+			cerr << "ERROR: Invalid argument in mtl file." << endl;
+			return materials;
+		}
+	}
+}
+
+/*
+* Parse lines until EOF or another material declaration
+*/
+int parse_material(Material* material)
+{
+	file.getline(line, 256);
+	return 1;
+}
--- src/material.h
+++ src/material.h
@@ -1,1 +1,34 @@
 #pragma once
+#define MATERIAL_H
+#include <vector>
+
+/*
+* .mtl Material information
+*/
+struct Material
+{
+	char* name; //'newmtl'
+	Color ambient; //'Ka' ambient 
+	Color diffuse; //'Kd' diffuse
+	Color specular; //'Ks' specular
+	Color transmission_filter; //'Tf' transmission filter
+	float specular_pow; //'Ns' specular exponent 
+	float optical_density; //'Ni' index of refraction
+	float dissolve; //'d' opacity
+	char* ambient_map; //'map_Ka' ambient map path
+	char* diffuse_map; //'map_Kd' diffuse map path
+	char* specular_map; //'map_Ks' specular map path
+	unsigned char illumination; //'illum'/'illum_#' illumination model
+};
+
+struct Color
+{
+	float r;
+	float g;
+	float b;
+};
+
+/*
+* Return vector of Materials from file
+*/
+std::vector<Material> parse_mtl(const char* path);
